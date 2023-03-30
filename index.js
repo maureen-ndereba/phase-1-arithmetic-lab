e114a749648637a0ad9e9735100764ad2cbeb2f9
mojustmo-glitch <maureen.ngugi@student.moringaschool.com>
2023-03-30 23:46:13 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 5
+let num4 = 29
+let mod = num4 % num3;
+
+let max = Math.max(12, 2, 13, 7, 20);
